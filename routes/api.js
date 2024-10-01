fa8de1663fb74b75229fe733b881f270295d6bf5
tunmph37268 <146613825+tunmph37268@users.noreply.github.com>
2024-10-01 17:52:34 +0700

    add image

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,39 @@ var router = express.Router();
 //Thêm model
 const Product =require("../models/product");
 const Suppliers =require("../models/suppliers");
-
+const Upload=require('../config/common/upload')
+//Thêm nhà cung cấp
+router.post('/add-supplier',Upload.single('image'),async(req,res)=>{
+    try{
+    const data=req.body;
+    const {file}=req
+    const urlsImage =file.map((file)=>`${req.protocol}://${req.get("host")}/uploads/${file.filename}`)
+    const newSuppliers =new Suppliers({
+      name:data.name,
+      phone:data.phone,
+      email:data.email,
+      description:data.description,
+      image:urlsImage
+    })
+    const result = await newSuppliers.save();
+            if (result) {
+                res.json({
+                   "status": 200,
+                    "message": "Thêm nhà cung cấp thành công",
+                   "data": result
+                });
+            } else {
+                res.json({
+                   "status": 400,
+                    "message": "Thất bại",
+                   "data": []
+                });
+            }
+    }catch(err){
+      console.log(err);
+    }
+    });
 //Thêm sản phẩm 
-router.post('/add-product',async(req,))
+// router.post('/add-product',async(req,))
 
 module.exports= router;
