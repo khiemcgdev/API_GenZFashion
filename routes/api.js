96d83a31d942e96cf8bf60011f7396c8788bb060
dieptvph40380 <dieptvph40380@fpt.edu.vn>
2024-10-01 17:10:15 +0700

    fix models

---
+++ routes/api.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+//Thêm model
+const Product =require("../models/product");
+const Suppliers =require("../models/suppliers");
+
+module.exports= router;
