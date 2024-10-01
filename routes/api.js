82b18fe366aab5caf7f7d0fbad751d7b00e3ece8
dieptvph40380 <dieptvph40380@fpt.edu.vn>
2024-10-01 17:53:54 +0700

    api add proeduct

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,41 @@ var router = express.Router();
 
 //Thêm model
 const Product =require("../models/product");
-const Suppliers =require("../models/suppliers");
+const Suppliers =require("../models/suppliers");    
 
 //Thêm sản phẩm 
-router.post('/add-product',async(req,))
+router.post('/add-product',Upload.single('image'),async(req,res)=>{
+    try{
+        const data =req.body;
+        const { file } = req
+        const newProduct=new Product({
+            quantity:data.quantity,
+            price:data.price,
+            description:data.description,
+            product_name:data.product_name,
+            image:`${req.protocol}://${req.get("host")}/uploads/${file.filename}`,
+            id_suppliers:data.id_suppliers,
+            id_producttype:data.id_producttype,  
+        });
+        const result=await newProduct.save();
+        if (result) {
+            //Nếu thêm thành công result !null trả về dữ liệu
+            res.json({
+              "status": 200,
+              "messenger": "Thêm thành công",
+              "data": result,
+            })
+          } else {
+            //Nếu thêm không thành công result null, thông báo không thành công
+            res.json({
+              "status": 400,
+              "messenger": "Lỗi,thêm không thành công",
+              "data": []
+            })
+          }
+    }catch(error){
+        console.log(error);
+    }
+});
 
 module.exports= router;
